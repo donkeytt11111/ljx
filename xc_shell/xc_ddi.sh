29cae92b0cad7b9653069a527d52b47dd5b3816c
jiaxin <167744103+donkeytt11111@users.noreply.github.com>
2024-05-28 14:37:21 +0800

    Update xc_ddi.sh

--- xc_shell/xc_ddi.sh
+++ xc_shell/xc_ddi.sh
@@ -198,7 +198,7 @@ pwd
 # 指定最大重试次数和等待时间
 MAX_RETRIES=100
 SLEEP_TIME=20
-MAX_error=10
+MAX_error=6
 # 计数器初始化
 retries=0
 
@@ -265,7 +265,8 @@ while true; do
   if (( retries > MAX_RETRIES )); then
     # 如果达到最大重试次数，打印警告并退出脚本
     echo "达到最大重试次数，仍有命名空间未达到Running状态。退出脚本。"
-    kubectl -n openebs  get pods | grep completed |awk '{print$1}'|xargs kubectl -n openebs delete pods
+    kubectl -n openebs  get pods | grep Completed |awk '{print$1}'|xargs kubectl -n openebs delete pods
+    break
   fi
 
   # 等待指定时间后再次检查
